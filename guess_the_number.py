85e99139d82c103478f6bbd08beff2de17e74ee8
Stevemats <stevematindi@gmail.com>
2023-07-28 14:19:50 +0300

    Added difficulty levels & max guess limit

--- guess_the_number.py
+++ guess_the_number.py
@@ -1,6 +1,17 @@
 import random
 import time
 
+class DifficultyLevel:
+    def __init__(self, name, lower_bound, upper_bound):
+        self.name = name
+        self.lower_bound = lower_bound
+        self.upper_bound = upper_bound
+
+# Define difficulty levels with their respective ranges
+EASY = DifficultyLevel("Easy", 1, 50)
+MEDIUM = DifficultyLevel("Medium", 1, 100)
+HARD = DifficultyLevel("Hard", 1, 200)
+
 def generate_guess(lower_bound, upper_bound):
     return random.randint(lower_bound, upper_bound)
 
@@ -23,29 +34,59 @@ def get_range_from_user():
         except ValueError:
             print("Invalid input. Please enter valid integers.")
 
+def get_max_guesses_from_user():
+    while True:
+        try:
+            max_guesses = int(input("Enter the maximum number of guesses allowed: "))
+            if max_guesses <= 0:
+                print("Invalid number of guesses. Please enter a positive integer.")
+            else:
+                return max_guesses
+        except ValueError:
+            print("Invalid input. Please enter a valid integer.")
+
+def select_difficulty_level():
+    print("\nSelect the difficulty level:")
+    print("1. Easy")
+    print("2. Medium")
+    print("3. Hard")
+    while True:
+        try:
+            user_choice = int(input("Enter the corresponding number (1/2/3): "))
+            if user_choice == 1:
+                return EASY
+            elif user_choice == 2:
+                return MEDIUM
+            elif user_choice == 3:
+                return HARD
+            else:
+                print("Invalid choice. Please enter 1, 2, or 3.")
+        except ValueError:
+            print("Invalid input. Please enter a number.")
+
 def main():
     print("Welcome to Guess the Number - AI Edition!")
-    print("Do you want to set the range of numbers for the AI to guess?")
-    user_input = input("Enter 'Y' for yes or 'N' for no: ").upper()
+    difficulty = select_difficulty_level()
 
-    if user_input == "Y":
-        lower_bound, upper_bound = get_range_from_user()
-    else:
-        lower_bound = 1
-        upper_bound = 100
+    lower_bound = difficulty.lower_bound
+    upper_bound = difficulty.upper_bound
 
     print("Think of a number between", lower_bound, "and", upper_bound)
-    time.sleep(2)  # Pause for 2 secs
+    time.sleep(2)
     print("I will try to guess the number you are thinking of.")
     time.sleep(1)
     print("You only need to respond with 'H' if the guess is too high, 'L' if it's too low, and 'C' if I guessed correctly.")
     time.sleep(2)
     print("Let's get started!")
 
-    while True:
+    max_guesses = get_max_guesses_from_user()
+    num_guesses = 0
+
+    while num_guesses < max_guesses:
         guess = generate_guess(lower_bound, upper_bound)
         print("AI's guess:", guess)
         user_response = get_user_feedback()
+        num_guesses += 1
 
         if user_response == "H":
             upper_bound = guess - 1
@@ -54,6 +95,8 @@ def main():
         else:  # user_response == "C"
             print("AI guessed correctly! The number was:", guess)
             break
+    else:
+        print("Sorry, the AI could not guess the correct number within the given number of guesses.")
 
 if __name__ == "__main__":
     main()
